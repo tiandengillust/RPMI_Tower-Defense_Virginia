fe32d008b487ba4e8f95101d5c61e89953ee2c7b
tiandeng <tiandengillust@gmail.com>
2021-04-15 12:06:29 +0200

    proyecto Tower Defense creado (primer nivel) - creación de nivel - disparo de torres - enemigo y navegación

---
+++ Assets/scripts/BulletMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [Tooltip("Velocidad de la bala")]
+    public float bulletSpeed;
+
+    [Tooltip("Referencia al enemigo que voy a matar")]
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, enemy.transform.position, bulletSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // La bala se destruye en cuanto colisiona con algún objeto
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/TowerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [Tooltip("Referencia al prefab de la bala")]
+    public GameObject bulletPrefab;
+
+    [Tooltip("Referencia al enemigo que voy a disparar")]
+    public GameObject enemy;
+
+    public float altura;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, altura, 0), Quaternion.identity);
+            bala.GetComponent<BulletMovement>().enemy = enemy;
+        }
+    }
+}
